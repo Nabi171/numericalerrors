0a007ed889ad4172715bcac2e235cbfa0fc23166
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-07-15 00:04:28 +0600

    changes in detaisPage section

--- src/components/pages/Books/BookDetails.tsx
+++ src/components/pages/Books/BookDetails.tsx
@@ -2,6 +2,7 @@
 import { useParams } from 'react-router-dom';
 import {useSingleBookQuery} from "../../../features/books/booksApi";
 import Loading from "../../ui/Loading"
+import { AiFillDelete } from 'react-icons/ai';
 
 export default function BookDetails({book}) {
     const { bookId } = useParams();
@@ -10,15 +11,25 @@ export default function BookDetails({book}) {
     // const { title, author, genre, publicationDate }=data;
         return (
             <>
-           { !data ? <Loading/> :  <div className="bg-white rounded-lg shadow-md p-4">
+            <div className="container mx-auto">
+           { !data ? <Loading/> :  <div className="bg-white rounded-lg shadow-md p-4 w-96 mx-auto mt-10">
          <h2 className="text-xl font-bold mb-2">{data?.title}</h2>
       <p className="text-gray-700 mb-2">Author:{data?.author} </p>
       <p className="text-gray-700 mb-2">Genre:{data?.genre} </p>
       <p className="text-gray-700 ">Publication Date: {data?.publicationDate}</p>
-          {/* <button className="bg-blue-500 hover:bg-blue-600 mt-3 text-white font-bold py-2 px-4 rounded ">
-          Details
-        </button> */}
+
+      <div className="flex justify-between">
+          <button className="bg-blue-500 hover:bg-blue-600 mt-3 text-white font-bold py-2 px-4 rounded ">
+          Edite
+        </button>
+ 
+        <button
+      className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+           <AiFillDelete/>
+       </button>
+      </div>
             </div>}
+            </div>
         </>
           );;
       }
