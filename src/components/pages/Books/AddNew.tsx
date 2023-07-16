a32894459785b7339abf1e8b0e6b4a80cd4f0527
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-07-16 18:36:41 +0600

    books details updated

--- src/components/pages/Books/AddNew.tsx
+++ src/components/pages/Books/AddNew.tsx
@@ -1,42 +1,78 @@
 /* eslint-disable prettier/prettier */
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useAppDispatch } from '../../../app/hooks';
 // import { addBook } from '../../../features/books/booksApi'
 import { useAddBookMutation } from '../../../features/books/booksApi';
 import { useNavigate } from 'react-router-dom';
 export default function AddNew() {
+  const [title, setTitle] = useState("");
+  // console.log('initial',initialTitle)
+  const [author, setAuthor] = useState("");
+  const [genre, setGenre] = useState("");
+  const [publicationDate, setPublicationDate] = useState("");
+
+
   const [addBook, { isLoading, isSuccess, isError }] = useAddBookMutation();
   const dispatch=useAppDispatch()
   const navigate = useNavigate();
-  const { register, handleSubmit, reset } = useForm();
+  // const { register, handleSubmit, reset } = useForm();
   
   const handleNavigate=()=>{
     navigate('/allbooks');
   }
 
-  const onSubmit = (data) => {
-    dispatch(addBook(data));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBook({
+      
+     
+          title,
+          author,
+          genre,
+          publicationDate,
+
+ 
+  });
+    
     navigate('/allbooks');
-    console.log(data)
-    alert('The book is added successfully')
+
   
-    reset(); // Reset the form after submission
-  };
+    alert('are you sure to add the book?')
+    window.location.reload();
+  
+
+};
+
+
+//another method using react hook form >optional
+  // const onSubmit = (data) => {
+  //   dispatch(addBook(data));
+  //   navigate('/allbooks');
+  //   console.log(data)
+  //   alert('The book is added successfully')
+  
+  //   reset(); // Reset the form after submission
+  // };
+
+
     return(
      <div>
         
         <form 
 
-        onSubmit={handleSubmit(onSubmit)} 
-        
+        // onSubmit={handleSubmit(onSubmit)} 
+        onSubmit={handleSubmit} 
         className="max-w-sm mx-auto">
       <div className="mb-4">
         <label htmlFor="title" className="block text-gray-700 text-sm font-bold mb-2">Title:</label>
         <input
           type="text"
           id="title"
-        
-          {...register('title', { required: true })}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          // {...register('title', { required: true })}
           className={`border focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md py-2 px-4`}
         />
         {/* {errors.title && <p className="text-red-500 text-xs mt-1">Title is required.</p>} */}
@@ -47,7 +83,9 @@ export default function AddNew() {
         <input
           type="text"
           id="author"
-          {...register('author', { required: true })}
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          // {...register('author', { required: true })}
           className={`border focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md py-2 px-4`}
         />
         {/* {errors.author && <p className="text-red-500 text-xs mt-1">Author is required.</p>} */}
@@ -58,7 +96,9 @@ export default function AddNew() {
         <input
           type="text"
           id="genre"
-          {...register('genre', { required: true })}
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+          // {...register('genre', { required: true })}
           className={`border focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md py-2 px-4`}
         />
         {/* {errors.genre && <p className="text-red-500 text-xs mt-1">Genre is required.</p>} */}
@@ -69,7 +109,9 @@ export default function AddNew() {
         <input
           type="date"
           id="publicationDate"
-          {...register('publicationDate', { required: true })}
+          value={publicationDate}
+          onChange={(e) => setPublicationDate(e.target.value)}
+          // {...register('publicationDate', { required: true })}
           className={`border focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md py-2 px-4`}
         />
         {/* {errors.publicationDate && <p className="text-red-500 text-xs mt-1">Publication Date is required.</p>} */}
--- src/components/pages/Books/AllBooks.tsx
+++ src/components/pages/Books/AllBooks.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import SingleBook from "./SingleBook"
+import Loading from "../../ui/Loading"
 import { useGetBooksQuery } from '../../../features/books/booksApi';
 export default function AllBooks() {
     const { data, isLoading, error } = useGetBooksQuery(undefined);
@@ -9,11 +10,19 @@ const latestPublicationDate = booksdata?.filter(item => new Date(item.publicatio
   .sort((a, b) => new Date(b.publicationDate) - new Date(a.publicationDate));
   console.log(latestPublicationDate)
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 container mx-auto mt-4">
+        <div >
+         { booksdata ? <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-4 gap-4 container mx-auto mt-4">
             {
               booksdata?.map(book=><SingleBook key={book._id} book={book} />)
             }
+         </div>
+         : 
+         <div className="ms-40 flex ">
+          <p className="text-white">Loading.........</p>
+         <Loading className=" text-white" ></Loading>
          
+         </div>
+         }
         </div>
       );;
   }
