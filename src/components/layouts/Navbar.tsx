946a29366394374fd003f14a283d875d3d388493
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-11-06 00:58:15 +0600

    changes in data again

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -25,52 +25,16 @@ const  handleLogout = (): void => {
   return (
     <nav className="bg-gray-800 py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="mx-auto">
           <div className="flex items-center">
+          <img src=" https://i.ibb.co/N3hGjRv/06-11-2023-00-54-17-REC-removebg-preview.png" alt="Your Logo" class="h-16  w-16 object-contain" />
             <div className="flex-shrink-0">
               <span className="text-white font-bold text-xl">Numerical Analysis Presentation</span>
             </div>
+           
+            
           </div>
-          {/* <div className="ml-4 flex items-center md:ml-6">
-          {user.email  &&  <Link
-              to="/allbooks"
-              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
-            >
-              All Books
-            </Link>}
-            {user.email && <Link
-              to="/addnew"
-              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
-            >
-              Add NewBook
-            </Link>}
-            {user.email && <Link
-              to="/wishlist"
-              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
-            >
-              WishList
-            </Link>}
-            { !user.email &&  <Link
-              to="/signup"
-              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
-            >
-              SignUp
-            </Link>}
-            { !user.email &&  <Link
-              to="/login"
-              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
-            >
-           SignIn
-            </Link>}
-            { user.email &&  <Link
-              to=""
-              onClick={handleLogout} 
-              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
-            >
-           SignOut
-            </Link>}
          
-          </div> */}
         </div>
       </div>
     </nav>
